b8952c7e28a2c880215c99caa3aea1993b7ef586
agizenezalove <kingbeat315@gmail.com>
2026-05-20 12:48:47 +0300

    second commit on ai button rotate

--- dash/src/components/Sidebar.jsx
+++ dash/src/components/Sidebar.jsx
@@ -112,14 +112,19 @@ function Sidebar({
         }
       >
         <div className="absolute bottom-6 left-1/2 -translate-x-1/2">
-          <div className="relative flex items-center justify-center">
-            
-            {/* OUTER ROTATING RING */}
-            <div className="w-10 h-10 border-2 border-yellow-400 border-t-transparent rounded-full animate-spin shadow-[0_0_20px_rgba(255,215,0,0.9)]"></div>
+          
+          <div className="relative w-12 h-12 flex items-center justify-center">
 
-            {/* INNER GLOW DOT */}
-            <div className="absolute w-3 h-3 bg-yellow-300 rounded-full animate-pulse shadow-[0_0_25px_rgba(255,215,0,1)]"></div>
+            {/* ROTATING GOLD RING BEHIND */}
+            <div className="absolute inset-0 rounded-full border-[3px] border-yellow-400 border-t-transparent animate-spin shadow-[0_0_25px_rgba(255,215,0,1)]"></div>
+
+            {/* AI LOGO CENTER */}
+            <div className="w-8 h-8 rounded-full bg-gradient-to-br from-yellow-300 to-yellow-500 flex items-center justify-center text-black font-black text-xs shadow-[0_0_20px_rgba(255,215,0,1)] z-10">
+              IA
+            </div>
+
           </div>
+
         </div>
       </button>
     </aside>
